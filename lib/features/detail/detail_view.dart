fb609ec1210611afff90b48ca09ce00affb5072e
Cesniux <cesniux@gmail.com>
2023-04-04 21:04:50 +0300

    stat bar finished

--- lib/features/detail/detail_view.dart
+++ lib/features/detail/detail_view.dart
@@ -11,8 +11,6 @@ class DetailView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const String statName = 'HP';
-    const String statValue = '45';
     return SafeArea(
       child: Scaffold(
         extendBodyBehindAppBar: true,
@@ -33,54 +31,95 @@ class DetailView extends StatelessWidget {
           ),
         ),
         body: Column(
-          children: [
-            const DetailPokemonImage(),
-            const DetailMainInfoPart(),
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const DetailStarIcon(),
-                      Text(
-                        'PERFORMANCE',
-                        style: textTheme.labelMedium,
-                      ),
-                      const DetailStarIcon()
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 10.w),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'HP',
-                        style: textTheme.labelSmall?.copyWith(fontSize: 18),
-                      ),
-                      SizedBox(
-                        width: 310.w,
-                        child: const ClipRRect(
-                          borderRadius: BorderRadius.all(Radius.circular(70)),
-                          child: LinearProgressIndicator(
-                            minHeight: 20,
-                            backgroundColor: CColors.silver,
-                            color: CColors.progressHp,
-                            value: 0.6,
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                )
-              ],
+          children: const [
+            DetailPokemonImage(),
+            DetailMainInfoPart(),
+            DetailPerformanceStats(
+              statAPIValue: 45,
+              statName: 'HP',
             )
           ],
         ),
       ),
+    );
+  }
+}
+
+class DetailPerformanceStats extends StatelessWidget {
+  final String statName;
+  final int statAPIValue;
+  const DetailPerformanceStats({
+    super.key,
+    required this.statName,
+    required this.statAPIValue,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    const int maxStatAPIValue = 300;
+    final int doubledStatAPIValue = statAPIValue * 2;
+    final double indicatorValue = (doubledStatAPIValue / maxStatAPIValue);
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const DetailStarIcon(),
+              Text(
+                'PERFORMANCE',
+                style: textTheme.labelMedium,
+              ),
+              const DetailStarIcon()
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10.w),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'HP',
+                style: textTheme.labelSmall?.copyWith(fontSize: 18),
+              ),
+              Stack(
+                children: [
+                  SizedBox(
+                    width: 310.w,
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.all(Radius.circular(70)),
+                      child: LinearProgressIndicator(
+                        minHeight: 23.h,
+                        backgroundColor: CColors.silver,
+                        color: CColors.progressHp,
+                        value: indicatorValue,
+                      ),
+                    ),
+                  ),
+                  Positioned.fill(
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10, left: 10),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          '$doubledStatAPIValue/$maxStatAPIValue',
+                          style: TextStyle(
+                            fontSize: 14.sp,
+                            fontStyle: FontStyle.italic,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
